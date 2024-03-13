39bb6dc5f2b718101c91a403a2bf578e77a5ba70
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-12 23:47:53 -0400

    dice's displaying, program's running

--- rolling_dice.py
+++ rolling_dice.py
@@ -7,7 +7,16 @@ window = Tk()
 window.geometry("500x250")
 window.title("Rolling Dice")
 
-button = Button(window, text="ROLL !!!", width=20, height=3, font=50, bg="white", bd=2)
+label = Label(window, text="", font=("times", 150))
+def dice():
+    dice = ["\u2680","\u2681", "\u2682", "\u2683", "\u2684", "\u2685"]
+    label.configure(text=f"{random.choice(dice)}")
+    label.pack()
+
+button = Button(window, text="ROLL !!!", width=20, height=3, font=50, bg="white", bd=2, command=dice)
 button.pack(padx=10, pady=10)
+
+
+
 
 window.mainloop()
