c07e32c4371f3272c37078e05ff40e7be6a0d5e8
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-12 23:17:05 -0400

    button's displaying

--- rolling_dice.py
+++ rolling_dice.py
@@ -7,7 +7,7 @@ window = Tk()
 window.geometry("500x250")
 window.title("Rolling Dice")
 
-
-
+button = Button(window, text="ROLL !!!", width=20, height=3, font=50, bg="white", bd=2)
+button.pack(padx=10, pady=10)
 
 window.mainloop()
